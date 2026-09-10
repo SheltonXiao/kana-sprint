79ef405807199521d5c4381fd7504b84653f13c2
Tong Xiao <57824954+SheltonXiao@users.noreply.github.com>
2026-09-10 01:23:33 -0700

    Add sightseeing vocabulary category

---
+++ data/vocab/sightseeing.js
@@ -0,0 +1,9 @@
+KanaData.vocab.push(
+  {j:'パンフレット',r:'panfuretto',c:'宣传册',src:'pamphlet',cat:'观光',w:3},
+  {j:'オーディオガイド',r:'oodio gaido',c:'语音导览',src:'audio guide',cat:'观光',w:2},
+  {j:'ガイドツアー',r:'gaido tsuaa',c:'导览团',src:'guided tour',cat:'观光',w:2},
+  {j:'ミュージアム',r:'myuujiamu',c:'博物馆/美术馆',src:'museum',cat:'观光',w:2},
+  {j:'ギャラリー',r:'gyararii',c:'画廊/展览空间',src:'gallery',cat:'观光',w:2},
+  {j:'フォトスポット',r:'foto supotto',c:'拍照点',src:'photo spot',cat:'观光',w:2},
+  {j:'チケットカウンター',r:'chiketto kauntaa',c:'售票柜台',src:'ticket counter',cat:'观光',w:2}
+);
