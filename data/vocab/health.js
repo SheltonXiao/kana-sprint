a1fbf1fb5268590baa6263a4560c7e5bcfbc0f78
Tong Xiao <57824954+SheltonXiao@users.noreply.github.com>
2026-09-10 01:23:40 -0700

    Add health vocabulary category

---
+++ data/vocab/health.js
@@ -0,0 +1,7 @@
+KanaData.vocab.push(
+  {j:'クリニック',r:'kurinikku',c:'诊所',src:'clinic',cat:'医疗',w:3},
+  {j:'ドクター',r:'dokutaa',c:'医生',src:'doctor',cat:'医疗',w:2},
+  {j:'ナース',r:'naasu',c:'护士',src:'nurse',cat:'医疗',w:2},
+  {j:'サプリメント',r:'sapurimento',c:'营养补充剂',src:'supplement',cat:'医疗',w:2},
+  {j:'コンタクトレンズ',r:'kontakuto renzu',c:'隐形眼镜',src:'contact lenses',cat:'医疗',w:2}
+);
